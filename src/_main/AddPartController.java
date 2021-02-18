0fced070a107f4fc3ec8bfc0707bf4fe66efbab9
pi <payam.farah@live.com>
2021-02-18 01:15:18 -0800

    requirement to have two different part type forms, and toggle between them completed

--- src/_main/AddPartController.java
+++ src/_main/AddPartController.java
@@ -1,8 +1,41 @@
 package _main;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
 public class AddPartController {
 
-    public void switchToOutsource(){
+    @FXML
+   // public RadioButton radioInHouse;
+   // public RadioButton radioOutsourced;
+    public Label partTypeSpecificField;
 
+
+// In-House parts are default.
+    public boolean isOutsourced = false;
+
+    /**
+     * These methods control the radio buttons
+     * to toggle part type form from In-House to
+     * Outsourced mode.
+     */
+
+    public void toggleIsOutsourced(){
+        if (isOutsourced == false){
+            isOutsourced = true;
+            partTypeSpecificField.setText("Company Name");
+        }
     }
+
+    public void toggleIsNotOutsourced(){
+        if (isOutsourced == true){
+            isOutsourced = false;
+          partTypeSpecificField.setText("Machine ID");
+      }
+    }
+
+
+
+
 }
